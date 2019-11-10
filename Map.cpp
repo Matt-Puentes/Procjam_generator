555d5a236882187e7a0e7d7e141f404f4f66c65c
Matthew Puentes <matt.puentes@gmail.com>
2019-11-09 19:14:31 -0500

    Lines have thickness

--- Map.cpp
+++ Map.cpp
@@ -2,6 +2,39 @@
 
 int normalRoomWidth = 100;
 
+class sfLine : public sf::Drawable
+{
+public:
+    sfLine(const sf::Vector2f& point1, const sf::Vector2f& point2):
+        color(sf::Color::Yellow), thickness(5.f)
+    {
+        sf::Vector2f direction = point2 - point1;
+        sf::Vector2f unitDirection = direction/std::sqrt(direction.x*direction.x+direction.y*direction.y);
+        sf::Vector2f unitPerpendicular(-unitDirection.y,unitDirection.x);
+
+        sf::Vector2f offset = (thickness/2.f)*unitPerpendicular;
+
+        vertices[0].position = point1 + offset;
+        vertices[1].position = point2 + offset;
+        vertices[2].position = point2 - offset;
+        vertices[3].position = point1 - offset;
+
+        for (int i=0; i<4; ++i)
+            vertices[i].color = color;
+    }
+
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const
+    {
+        target.draw(vertices,4,sf::Quads);
+    }
+
+
+private:
+    sf::Vertex vertices[4];
+    float thickness;
+    sf::Color color;
+};
+
 Map::Map(){
     rooms.clear();
     usedPositions = std::vector<sf::Vector2f>();
@@ -87,17 +120,26 @@ float Map::drawToWindow(sf::RenderTarget *window, int smallest_x, int smallest_y
     printf("Gonna draw the map!\n");
     for(int i = 0; i < rooms.size(); i++){
         for(int j = 0; j < rooms[i] -> getNeighbors().size(); j++){
+            
             if(rooms[i] -> getNeighbors()[j] != NULL){
-                sf::Vertex  linex[] = {
-                    sf::Vertex(sf::Vector2f(rooms[i]->getPos().x - smallest_x, rooms[i]->getPos().y - smallest_y)),
-                    sf::Vertex(sf::Vector2f(rooms[i]->getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y))
-                };
-                sf::Vertex  liney[] = {
-                    sf::Vertex(sf::Vector2f(rooms[i]->getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y)),
-                    sf::Vertex(sf::Vector2f(rooms[i] -> getNeighbors()[j] -> getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y))
-                };
-                window->draw(linex, 2, sf::Lines);
-                window->draw(liney, 2, sf::Lines);
+                // sf::Vertex  linex[] = {
+                //     sf::Vertex(sf::Vector2f(rooms[i]->getPos().x - smallest_x, rooms[i]->getPos().y - smallest_y)),
+                //     sf::Vertex(sf::Vector2f(rooms[i]->getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y))
+                // };
+                // sf::Vertex  liney[] = {
+                //     sf::Vertex(sf::Vector2f(rooms[i]->getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y)),
+                //     sf::Vertex(sf::Vector2f(rooms[i] -> getNeighbors()[j] -> getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y))
+                // };
+                sf::Vector2f l1v1(rooms[i]->getPos().x - smallest_x, rooms[i]->getPos().y - smallest_y);
+                sf::Vector2f l1v2(rooms[i]->getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y);
+                sf::Vector2f l2v1(rooms[i]->getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y);
+                sf::Vector2f l2v2(rooms[i] -> getNeighbors()[j] -> getPos().x - smallest_x, rooms[i] -> getNeighbors()[j] -> getPos().y - smallest_y);
+                sfLine line1 = sfLine(l1v1, l1v2);
+                sfLine line2 = sfLine(l2v1, l2v2);
+                window->draw(line1);
+                window->draw(line2);
+                // window->draw(linex, 2, sf::Lines);
+                // window->draw(liney, 2, sf::Lines);
             }
         }
     }
